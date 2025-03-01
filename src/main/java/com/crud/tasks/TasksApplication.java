36e05e780f7ab20820f94632625623d69ba39442
Roman <roman.adamczyk12@gmail.com>
2025-03-01 16:49:02 +0100

    22.2 module

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,11 +3,13 @@ package com.crud.tasks;
 import com.crud.tasks.controller.TrelloController;
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.trello.client.TrelloClient;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
 
+@OpenAPIDefinition
 @SpringBootApplication
 public class TasksApplication {
 
