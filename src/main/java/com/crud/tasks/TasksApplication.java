2430754e5b1c45e37a68edc2dde9e7864d7d0bad
Roman <roman.adamczyk12@gmail.com>
2025-03-02 11:38:06 +0100

    22.4 module instructions plus task

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -7,8 +7,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
+@EnableScheduling
 @OpenAPIDefinition
 @SpringBootApplication
 public class TasksApplication {
